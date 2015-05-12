d45745099067a964cfdc0b9fa623d9bd75809838
SLeonov <ragioniere1980@gmail.com>
2015-05-12 22:38:15 +0300

    mvc how it is + pdo

--- www/classes/DB.php
+++ www/classes/DB.php
@@ -6,35 +6,13 @@ class DB
 
     public function __construct()
     {
-//        mysql_connect('localhost','root','');
-//        mysql_select_db('test');
-
         $this->dbh = new PDO('mysql:dbname=test;host=localhost', 'root', '');
     }
 
     public function query($sql, $param=[])
-{
-    $sth = $this->dbh->prepare($sql);
-    $sth->execute($param);
-    return $sth->fetchAll();
+    {
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute($param);
+        return $sth->fetchAll(PDO::FETCH_OBJ);
+    }
 }
-    /*
-    public function queryAll($sql, $class = 'stdClass')
-    {
-        $res = mysql_query($sql);
-        if (false === $res){
-            return false;
-        }
-        $ret = [];
-        while($row = mysql_fetch_object($res, $class)){
-            $ret[] = $row;
-        }
-        return $ret;
-    }
-
-    public function queryOne($sql, $class = 'stdClass')
-    {
-        return $this->queryAll($sql, $class)[0];
-    }
-    */
-} 
