9897723e724ec896f2e3f48842ab405b3969091f
SLeonov <ragioniere1980@gmail.com>
2015-05-17 09:54:51 +0300

    метод поиска по title

--- www/classes/AbstractModel.php
+++ www/classes/AbstractModel.php
@@ -16,6 +16,11 @@ abstract class AbstractModel
         return $this->data[$k];
     }
 
+    public function __isset($k)
+    {
+        return isset($this->data[$k]);
+    }
+
     public static function findAll()
     {
         $class = get_called_class();
@@ -34,6 +39,18 @@ abstract class AbstractModel
         return $db->query($sql, [':id' => $id])[0];
     }
 
+    public static function findOneByColumn($column, $value)
+    {
+        $db = new DB();
+        $db->setClassName(get_called_class());
+        $sql = 'SELECT * FROM ' .  static::$table . ' WHERE ' . $column . '=:value';
+        $res =  $db->query($sql, [':value' => $value]);
+        if (!empty($res)){
+            return $res[0];
+        }
+        return false;
+
+    }
     public function insert()
     {
         $cols = array_keys($this->data);
--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -8,39 +8,9 @@ class NewsController
 {
     public function actionAll()
     {
-//        $db = new DB();
-//        $res = $db->query(
-//            'SELECT * FROM news WHERE id=:id',
-//            [':id' => 1]);
+        $article = NewsModel::findOneByColumn('title', 'Привет');
 
-        $article = new NewsModel();
-        $article->title = 'Пурум пум пум';
-        $article->text = 'Еще одна проверка';
-        $article->insert();
+        var_dump($article);
 
-
-//        var_dump(
-//        NewsModel::findOneByPk(1)
-//    );
-//        die;
-//        echo NewsModel::getTable();
-//        die;
-        /*
-        $news = News::getAll();
-        $view = new View();
-        $view->items = $news;
-        $view->display('news/all.php');
-        */
     }
-
-    public function actionOne()
-    {
-
-        $id = $_GET['id'];
-        $item = News::getOne($id);
-        $view = new View();
-        $view->assign('items', $item);
-        $view->display('news/one.php');
-    }
-
-} 
+}
