017c1da4ee2fc10c72ad2e9eb5bc9cb53cdb2438
SLeonov <ragioniere1980@gmail.com>
2015-05-20 18:55:11 +0300

    htaccess

--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -8,7 +8,18 @@ class NewsController
 {
     public function actionAll()
     {
+        $news = NewsModel::findAll();
+        $view = new View();
+        $view->items = $news;
+        $view->display('news/all.php');
+    }
 
-            $art = NewsModel::findOneByColumn('title', 'Новый заголовок 123');
+    public function actionOne()
+    {
+        $id = $_GET['id'];
+        $item = NewsModel::getOne($id);
+        $view = new View;
+        $view->assign('item', $item);
+        $view->display('news/one.php');
     }
 }
