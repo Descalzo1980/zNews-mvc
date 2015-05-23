f7252410db3a3cffd282b187d8828f6659d42825
SLeonov <ragioniere1980@gmail.com>
2015-05-23 11:41:07 +0300

    view

--- www/views/news/all.php
+++ www/views/news/all.php
@@ -1,5 +1,17 @@
-<?php foreach ($items as $item):?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Новости украшают наш быт</title>
+</head>
+<body>
 
-    <h1><?php echo $item->title; ?></h1>
-    <div><?php echo $item->text; ?></div>
+<?php foreach ($items as $item): ?>
+    <article>
+        <h1><?php echo $item->title; ?></h1>
+        <div><?php echo $item->text; ?></div>
+    </article>
 <?php endforeach; ?>
+
+</body>
+</html>
